ac5fe25066255be04ec362cc013ddd285004dbe5
sCarita <gustavosilvacarita@gmail.com>
2023-01-05 22:49:13 +0000

    refactor: added comments with detail description of backend runtime operations.

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -4,29 +4,36 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import cors from "cors";
 import { z } from "zod";
 
+// Interfact Definition
 interface ChatMessage {
   user: string;
   message: string;
 }
-
+// Backend Constants
+// - variable "messages" holds an array of "ChatMessage" interfaces.
 const messages: ChatMessage[] = [
   { user: "user1", message: "Hello" },
   { user: "user2", message: "Hi" },
 ];
-
+// Application tRPC router - Handler of variable "messages".
 const appRouter = trpc
   .router()
+  // - query route "hello" to retrive static information.
   .query("hello", {
     resolve() {
       return "Hello world III";
     },
   })
+  // - query route "getMessages" to acquire the values of our backend 
+  //   variable "messages".
   .query("getMessages", {
     input: z.number().default(10),
     resolve({ input }) {
       return messages.slice(-input);
     },
   })
+  // - mutation route "addMessage" to acquire add a new "ChatMessage" instance
+  //   into of our backend variable "messages".
   .mutation("addMessage", {
     input: z.object({
       user: z.string(),
@@ -39,11 +46,11 @@ const appRouter = trpc
   });
 
 export type AppRouter = typeof appRouter;
-
+// Create an Express server instance.
 const app = express();
 app.use(cors());
 const port = 8080;
-
+// Add tRCP routes to our express http server.
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
